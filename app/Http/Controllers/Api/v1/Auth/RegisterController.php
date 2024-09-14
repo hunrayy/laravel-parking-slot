3b06fe3a9b4cb363566cde783ba15ca185b70e27
hunrayy <henryokiyi8@gmail.com>
2024-09-14 23:44:21 +0100

    first commit

---
+++ app/Http/Controllers/Api/v1/Auth/RegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterController extends Controller
+{
+    //
+    public function __invoke(Request $request){
+        // $user = $request->name;
+        // $device = substr($request->userAgent() ?? "", 0, 255);
+        // $token = $user->createToken($device)->plainTextToken;
+        // return response()->json([
+        //     "user" => $user,
+        //     "token" => $token
+        // ]);
+        
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ]);
+        // return $request;
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        event(new Registered($user));
+        $device = substr($request->userAgent() ?? '', 0, 255);
+        
+        return response()->json([
+            'access_token' => $user->createToken($device)->plainTextToken,
+        ], Response::HTTP_CREATED);
+        
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\Auth;
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('auth/register', Auth\RegisterController::class);
